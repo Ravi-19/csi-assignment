29dfda591c6ad5a57cef4702dfc05106c207790b
ravi <ravishekhawatofficial@gmail.com>
2024-05-24 17:42:31 +0530

    add some comment

--- assignment1_index.js
+++ assignment1_index.js
@@ -1,10 +1,12 @@
 ///here is a quick demo for simple hello world app 
-// 
+// importing https server 
 const http = require('http');
 
+// creating server using http module 
 http.createServer( (req, res)=> {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
 }).listen(4000 , ()=> {
+    // listing on server 4000 
     console.log("listing on port " , 4000) ; 
 });
