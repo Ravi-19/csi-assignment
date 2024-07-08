42ee04ef6afdea7782acb4b558bd233720ebe193
ravi <ravishekhawatofficial@gmail.com>
2024-07-08 18:04:18 +0530

    fix some bug

--- assignment5/index.js
+++ assignment5/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const items = require('./routes/items');
+require('dotenv').config() ; 
 
 const app = express();
 const port = 3000;
